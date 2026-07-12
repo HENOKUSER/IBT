1947bb81b75882beb482a17dcb109729d9be2de4
Henok Dubale <henanek716@gmail.com>
2026-07-12 16:07:57 +0300

    week 1 day 1 python  practice printing ,data type

--- week-1/programmmingFundamental.py
+++ week-1/programmmingFundamental.py
@@ -2,3 +2,66 @@
 print(1+3)              #  out put is 4
 print("hello world")    #  out put is hello
 
+#  built in function 
+# 1. print()
+print ("print is used Prints text or variables to the console.")
+# . input()
+inputExample = input("Enter your name: ")
+print(f"Hello, {inputExample}! see input let user input data")  # try hat input does.
+
+# 3. abs(x)
+print(f"abs() is used to return the absolute value of a number: {abs(-10)}")
+
+
+# 4. type()
+print(f"type() is used to return the type of an object: {type(10)}")
+
+# 5. len()
+print(f"len() is used to return the length of an object: {len('hello world')}")
+
+# 6. str()
+print(f"str() is used to convert an object to a string: {str(10)}")
+
+# their are also list of built in function which we will see in their respective topic.
+
+
+# data types
+# 1. number   int, float, complex ,list, tuple, set, dict, bool, str        
+
+print("data types are used to define the type of data we are working with in python. ")
+
+# 1. int
+print(f"1. type(10): {type(10)}")  # int is used to represent whole numbers
+
+# 2. float
+print(f"2. type(10.5): {type(10.5)}")  # float is used to represent decimal numbers
+
+# 3. complex
+print(f"3. type(1 + 2j): {type(1 + 2j)}")  # complex is used to represent complex numbers     
+
+# 4 string
+print(f"4. type('hello world'): {type('hello world')}")  # string is used to represent text data 
+
+
+# 5. list
+print(f"5. type([1, 2, 3]): {type([1, 2, 3])}")  # list is used to represent a collection of items    
+print(f"6. type(['sami', 'ali', 'ahmed']): {type(['sami', 'ali', 'ahmed'])}")  # list is used to represent a collection of items   
+
+# 6. tuple
+print(f"7. type((1, 2, 3)): {type((1, 2, 3))}")  # tuple is used to represent a collection of items that cannot be changed   
+
+# 7 set
+print(f"8. type({1, 2, 3}): {type({1, 2, 3})}")  # set is used to represent a collection of unique items     
+
+# 8 dictionary          
+print(f"9. type({{'name': 'sami', 'age': 23}}): {type({'name': 'sami', 'age': 23})}")  # dict is used to represent a collection of key-value pairs   
+
+
+#  variables
+# 1. variable is a name that holds a value
+
+a=10
+print(f"a = {a}")  # 10
+
+first_name = "sami"
+print(f"first_name = {first_name}")  # sami
